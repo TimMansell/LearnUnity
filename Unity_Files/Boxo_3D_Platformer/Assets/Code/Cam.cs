dda5310a64e4d1f2fb11c8bbc2116a2c0b928161
TimMansell <mansell.tim@gmail.com>
2018-09-05 20:15:36 +1000

    Chapter 4: Setup up camera to follow player

---
+++ Unity_Files/Boxo_3D_Platformer/Assets/Code/Cam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour {
+
+    public GameObject Player;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - Player.transform.position;
+	}
+
+    private void LateUpdate()
+    {
+        transform.position = Player.transform.position + offset;
+    }
+}
