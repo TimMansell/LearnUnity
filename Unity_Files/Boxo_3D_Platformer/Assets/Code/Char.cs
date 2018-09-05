c4f5450f097c8ce0819200d103fa097756190ac9
TimMansell <mansell.tim@gmail.com>
2018-09-05 18:35:59 +1000

    Chapter 4: Add character and create c# code to move character

---
+++ Unity_Files/Boxo_3D_Platformer/Assets/Code/Char.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char : MonoBehaviour {
+    public float Speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * Time.deltaTime * Speed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * Time.deltaTime * Speed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * Time.deltaTime * Speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * Time.deltaTime * Speed;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
